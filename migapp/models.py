f485e2c13b4f6659c1ff7cef4134c21f520fa774
uuso <mymail.uuso@gmail.com>
2019-12-06 13:21:01 +0300

    easiest app

--- migapp/models.py
+++ migapp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class GreatObject(models.Model):
+    title = models.TextField()
+    value = models.SmallIntegerField(default=0)
+    def __str__(self):
+        return '%s -- %d' % (self.title, self.value)
--- migapp/views.py
+++ migapp/views.py
@@ -1,3 +1,12 @@
+from django.http import HttpResponse
+from django.template import loader
 from django.shortcuts import render
+from migapp.models import GreatObject
 
 # Create your views here.
+
+def easiest(request):
+    gos = GreatObject.objects.all()
+    data = {'objects': gos, }
+    template = loader.get_template('easy.html')
+    return HttpResponse(template.render(data))
